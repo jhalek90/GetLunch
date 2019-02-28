27dccefd536b784422231d6346358e3f0c614a6a
jhalek90 <justinhalek@gmail.com>
2019-02-28 12:55:56 -0600

    Initial upload
    -

---
+++ GetLunch.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+link[0]="https://www.ubereats.com/en-US/austin/food-delivery/the-originals-salvatex-cuisine-homemade-salvadorian-%26-mexican-food/B1tN5oddQLS9p1TYaoqUaw/"
+name[0]="Salvatex"
+link[1]="https://www.ubereats.com/en-US/austin/food-delivery/bamboo-bistro-west-gate/JvMILkdnRza6wYa3g0K5tQ/"
+name[1]="Bamboo Bistro"
+link[2]="https://www.ubereats.com/en-US/austin/food-delivery/cafe-blue-downtown/xTGSKRrSSMafX2R36xfrDg/"
+name[2]="Cafe Blue"
+link[3]="https://www.ubereats.com/en-US/austin/food-delivery/austin-daily-press-cesar-chavez/wiBfpKKrSt6HlyaWiowmBQ/"
+name[3]="Austin Daily Press"
+link[4]="https://www.ubereats.com/en-US/austin/food-delivery/the-vegan-nom/wXGp1-NLSGO4AI5VBmK3ig/"
+name[4]="The Vegan Nom"
+link[5]="https://www.ubereats.com/en-US/austin/food-delivery/phoenician-resto-cafe/PZVhYKm-RuSe6bBeB5NnZw/"
+name[5]="Phoenician" 
+link[6]="https://www.ubereats.com/en-US/austin/food-delivery/southside-flying-pizza-cesar-chavez/Mkml1vn9SJGoFn4NQzGRsQ/"
+name[6]="Southside Flying Pizza"
+link[7]="https://www.ubereats.com/en-US/austin/food-delivery/con-madre-kitchen/46GCWwGOQo6Zh5xA-FqI5Q/"
+name[7]="Con Madre"
+link[8]="https://www.ubereats.com/en-US/austin/food-delivery/ya-hayati/GZ57TmsRRYeafwFm998X9w/"
+name[8]="Ya Hayati"
+link[9]="https://www.ubereats.com/en-US/austin/food-delivery/taqueria-chapala-cesar-chavez/S0Ec3KayTZyjcT9aezORGA/"
+name[9]="Taqueria Chapala"
+link[10]="https://www.ubereats.com/en-US/austin/food-delivery/flyrite-chicken-e-7th/vPFB4djDRPuoXKpmiTxJRQ/"
+name[10]="FlyRite"
+link[11]="https://www.ubereats.com/en-US/austin/food-delivery/tropicana-cuban-restaurants/44c8We-SREqVagekyQ5zLA/"
+name[11]="Tropicana"
+link[12]="https://www.ubereats.com/en-US/austin/food-delivery/which-wich-mueller/Itnn9TkXTHykeTexoJqx8Q/"
+name[12]="Which Wich"
+link[13]="https://www.ubereats.com/en-US/austin/food-delivery/mour-cafe/sBNryHIaQgCyaXiQEsYFeQ/"
+name[13]="Mour Cafe"
+link[14]="https://www.ubereats.com/en-US/austin/food-delivery/spartan-pizza/J6KE-AO4SeG3yXY33QZmAA/"
+name[14]="Spartan Pizza"
+link[15]="https://www.ubereats.com/en-US/austin/food-delivery/phoebes-diner/uPv4iJCRQ7CwcJOLS64ohw/"
+name[15]="Phobes"
+link[16]="https://www.ubereats.com/en-US/austin/food-delivery/tx-shawarma/XBXDwhZtRbenJbUw5q8s1Q/"
+name[16]="Tx Shawarma"
+link[17]="https://www.ubereats.com/en-US/austin/food-delivery/mission-dogs/RMmEUp75T1GKWL15bu3Kow/"
+name[17]="Mission Dogs"
+link[18]="https://www.ubereats.com/en-US/austin/food-delivery/baod-up/aDmkdQ-iR_OVXkrzvMJUvw/"
+name[18]="Boad Up"
+link[19]="https://www.ubereats.com/en-US/austin/food-delivery/vivo/mz-_hM9bS86y01u3RzYVmQ/"
+name[19]="Vivo"
+link[20]="https://www.ubereats.com/en-US/austin/food-delivery/crossroads-farm-to-truck/t-fJuFKaSj-2X8ikvzXhPg/"
+name[20]="Cross Roads"
+link[21]="https://www.ubereats.com/en-US/austin/food-delivery/donns-bbq/9W_fiIr_Q2-D_-bXucP47g/"
+name[21]="Donns BBQ"
+link[22]="https://www.ubereats.com/austin/food-delivery/blue-star-kitchen-%2B-bar/HuL9xbUjQCyCy2rccGwtkg/"
+name[22]="Blue Star"
+link[23]="https://www.ubereats.com/en-US/austin/food-delivery/sushi-zushi-5th-st/TNGYHKtwSO-YlsTNrr_QeQ/"
+name[23]="Sushi Zushi"
+link[24]="https://www.ubereats.com/austin/food-delivery/clay-pit/bQ7SLDq5RYmMNyRk-hyp9A/"
+name[24]="Clay Pit"
+link[25]="https://www.ubereats.com/stores/9b2ad25d-aa6d-4921-8d9e-0642db5fc0cd/"
+name[25]="Milano Cafe"
+link[26]="https://www.ubereats.com/austin/food-delivery/look-poke/EpE50yXZSTKgSu_opURRoQ/"
+name[26]="Look Poke"
+link[27]="https://www.ubereats.com/en-US/austin/food-delivery/henbit/hcFRfKdHQHClV0Dl5o0ZXA/"
+name[27]="HenBit"
+link[28]="https://www.ubereats.com/austin/food-delivery/34th-street-cafe/YYf0amwBQcaovFg5wKlkhw/"
+name[28]="34th Street Cafe"
+link[29]="https://www.ubereats.com/en-US/austin/food-delivery/conscious-cravings-south-1st/_UoGc9HGTl2gZi86ozxb8w/"
+name[29]="Concious Cravings"
+link[30]="https://www.ubereats.com/en-US/austin/food-delivery/hopdoddy-burger-bar-congress-ave/gOANncQXSU6jTYzJD9aY3A/"
+name[30]="Hop Doddy Burgers"
+
+link[31]="https://www.ubereats.com/en-US/austin/food-delivery/eastside-tavern/uG9sVvVgSSasKV8A0hLFVA/"
+name[31]="EastSide Tavern"
+link[32]="https://www.ubereats.com/en-US/austin/food-delivery/docs-backyard/PYmzSEBnSIaiVTKrT0dx0A/"
+name[32]="Docs Backyard"
+link[33]="https://www.ubereats.com/en-US/austin/food-delivery/green-mesquite-bbq-southpark-meadows/bxRgWU_PTV--bx1tHvZuFg/"
+name[33]="Green Mesquite BBQ "
+link[34]="https://www.ubereats.com/en-US/austin/food-delivery/lazarus-brewing-company/JoJkZZIDSTOU-eR-A-3hsw/"
+name[34]="Lazarus Brewing Company"
+link[35]="https://www.ubereats.com/en-US/austin/food-delivery/iron-cactus-downtown/INYAXw-eQYqE2xqGOwl2nw/"
+name[35]="Iron Cactus"
+link[36]="https://www.ubereats.com/en-US/austin/food-delivery/sawyer-%26-co/njOuRIPWRYuZ49w0CoqA5A/"
+name[36]="Sawyer Co"
+link[37]="https://www.ubereats.com/en-US/austin/food-delivery/black-sheep-lodge/UFx_tVxnT7WKkILpZXcO8A/"
+name[37]="Black Sheep"
+link[38]="https://www.ubereats.com/en-US/austin/food-delivery/reds-porch/61spu69xRPGmHDxMbK5vYA/"
+name[38]="Reds Porch"
+link[39]="https://www.ubereats.com/en-US/austin/food-delivery/oakwood-bbq/aQYL1X6VTkuFN7EtrXq_Mg/"
+name[39]="Oakwood BBQ"
+link[40]="https://www.ubereats.com/en-US/austin/food-delivery/big-easy-bar-and-grill-the-original-new-orleans-po-boy-and-gumbo-shop/2hGZ7p45SdCOHwl0mmMJNQ/"
+name[40]="Big Easy"
+
+link[41]="https://www.ubereats.com/en-US/austin/food-delivery/johnny-carinos-austin-slaughter/ehmWgrOGTuqhhD3uVNZBbw/"
+name[41]="Johnny Carino"
+link[42]="https://www.ubereats.com/en-US/austin/food-delivery/tejis-indian-restaurant/MQwtZH33R0GaXZ6MylpENA/"
+name[42]="Tejis Indian Restaurant"
+link[43]="https://www.ubereats.com/en-US/austin/food-delivery/bombay-bistro-s-lamar/BDzcG7nRSiW9JA1uxfZPaA/"
+name[43]="Bombay Bistro"
+link[44]="https://www.ubereats.com/en-US/austin/food-delivery/k-bop/Qk2DSR5uTUypFHRFniUT3g/"
+name[44]="K-Bop"
+link[45]="https://www.ubereats.com/en-US/austin/food-delivery/kismet-cafe-austin/sMz9tg0_R52A7FTIaQYFbA/"
+name[45]="Kismet Cafe"
+link[46]="https://www.ubereats.com/en-US/austin/food-delivery/noon-mediterranean-riverside/90r0vb_oSmmdqyaVhtZcRg/"
+name[46]="Noon"
+link[47]="https://www.ubereats.com/en-US/austin/food-delivery/austin-foody-moody/LJMcZlz4QuGIncYXJ603Wg/"
+name[47]="Foody Moody"
+link[48]="https://www.ubereats.com/en-US/austin/food-delivery/stinsons-bistro/oUKN9KRKRaCthUp4A55N5Q/"
+name[48]="Stinsons Bistro"
+link[49]="https://www.ubereats.com/en-US/austin/food-delivery/pad-thai-cuisine/VMSxiOOzRCeiLSi6zhLamw/"
+name[49]="Pad Thai Cuisine"
+link[50]="https://www.ubereats.com/en-US/austin/food-delivery/kim-phung-restaurant-riverside/JU233dJ1Rom8OWCZqyesUQ/"
+name[50]="Kim Phung Restaurant"
+link[51]="https://www.ubereats.com/en-US/austin/food-delivery/halcyon/wNgGrZiZSyu_Gv8Lw-qvbA/"
+name[51]="Halcyon"
+
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+checkAvailability () {
+  Result="Avalible"
+  if [[ $1 == *"Opens"* ]] || [[ $1 == *"Currently unavailable"* ]]; then
+    Result="Closed"
+  fi
+}
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [ -n "$1" ]; then
+  NumberOfPlaces=$1
+else
+  echo -e "$YELLOW ./GetLunch <Number of places>"
+  exit 1
+fi
+
+#-----Logo-----#
+clear
+printf "${GREEN}"
+cat logo.txt
+printf "${NC}"
+#-----Logo-----#
+
+echo -e "${NC}Checking$PURPLE UberEats${NC} for avalible places..."
+echo -e "${NC}"
+
+
+
+p=() #Empty array
+index=0
+
+
+until  [ $index == $NumberOfPlaces ]; do
+  Result=" "
+  until  [ "$Result" == "Avalible" ]; do
+    rand=$[$RANDOM % ${#link[@]}]
+    Url=${link[$rand]}
+    PlaceName=${name[$rand]}
+    checkAvailability "$(w3m -r -dump -l 1 "$Url")"
+  done
+
+  str="${YELLOW}$PlaceName ${NC}: ${GREEN}$Url ${NC}"
+  containsElement "$str" "${p[@]}"
+  if [ "$?" == "1" ]; then
+    p[$index]=$str
+    echo -e "    - $str"
+    let "index+=1"
+  fi
+done
+
+echo -e "${NC}"
+echo -e "$PURPLE Control click links to open them in a new tab"
+echo -e "${NC}"
